f0a2be1bfc2b0725805b51def3f2cff9951105fe
grongierisc <guillaume.rongier@intersystems.com>
2020-09-26 23:16:42 +0200

    Init endpoint

--- front/src/app/endpoints/endpoints.component.ts
+++ front/src/app/endpoints/endpoints.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Tenant } from '../fhiraas-api/model/tenant';
+import { Endpoint } from '../fhiraas-api/model/Endpoint';
+import { ActivatedRoute } from '@angular/router';
+import { DefaultService } from '../fhiraas-api';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-endpoints',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EndpointsComponent implements OnInit {
 
-  constructor() { }
+  tenant : Tenant;
+  endpoints : Endpoint[];
+  endpoint : Endpoint;
+
+  constructor(    
+    private route: ActivatedRoute,
+    private fhiraaService: DefaultService,
+    private location: Location) {  }
 
   ngOnInit(): void {
+    this.getTenant();
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
+  getTenant(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.fhiraaService.getTenant(id)
+      .subscribe(tenant => this.tenant = tenant);
+  }
 }
